5eb8babb38f7cb1d193044f77f091f937db02310
İsmail İliksiz <ismail.iliksiz@mrevyap.k12.tr>
2021-11-03 10:06:55 +0300

    Add project files.

---
+++ asansör uygulamasi/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace asansör_uygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.kat4 = new System.Windows.Forms.Button();
+            this.kat3 = new System.Windows.Forms.Button();
+            this.kat2 = new System.Windows.Forms.Button();
+            this.kat1 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.kat4);
+            this.groupBox1.Controls.Add(this.kat3);
+            this.groupBox1.Controls.Add(this.kat2);
+            this.groupBox1.Controls.Add(this.kat1);
+            this.groupBox1.Location = new System.Drawing.Point(223, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(188, 116);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Katlar";
+            // 
+            // kat4
+            // 
+            this.kat4.Location = new System.Drawing.Point(100, 63);
+            this.kat4.Name = "kat4";
+            this.kat4.Size = new System.Drawing.Size(70, 33);
+            this.kat4.TabIndex = 3;
+            this.kat4.Text = "Kat4";
+            this.kat4.UseVisualStyleBackColor = true;
+            this.kat4.Click += new System.EventHandler(this.kat4_Click);
+            // 
+            // kat3
+            // 
+            this.kat3.Location = new System.Drawing.Point(19, 63);
+            this.kat3.Name = "kat3";
+            this.kat3.Size = new System.Drawing.Size(70, 33);
+            this.kat3.TabIndex = 2;
+            this.kat3.Text = "Kat 3";
+            this.kat3.UseVisualStyleBackColor = true;
+            this.kat3.Click += new System.EventHandler(this.kat3_Click);
+            // 
+            // kat2
+            // 
+            this.kat2.Location = new System.Drawing.Point(100, 23);
+            this.kat2.Name = "kat2";
+            this.kat2.Size = new System.Drawing.Size(70, 33);
+            this.kat2.TabIndex = 1;
+            this.kat2.Text = "Kat 2";
+            this.kat2.UseVisualStyleBackColor = true;
+            this.kat2.Click += new System.EventHandler(this.kat2_Click);
+            // 
+            // kat1
+            // 
+            this.kat1.Location = new System.Drawing.Point(19, 23);
+            this.kat1.Name = "kat1";
+            this.kat1.Size = new System.Drawing.Size(70, 33);
+            this.kat1.TabIndex = 0;
+            this.kat1.Text = "Kat 1";
+            this.kat1.UseVisualStyleBackColor = true;
+            this.kat1.Click += new System.EventHandler(this.kat1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(264, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Asansörü Çağır";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(190, 206);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Bulunduğunuz Kat:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(220, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "1.Asansör:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(320, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "2.Asansör:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(283, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "_";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(380, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "_";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(283, 173);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "_______";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(293, 203);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(84, 21);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(691, 348);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button kat4;
+        private System.Windows.Forms.Button kat3;
+        private System.Windows.Forms.Button kat2;
+        private System.Windows.Forms.Button kat1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ asansör uygulamasi/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace asansör_uygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("1");
+            comboBox1.Items.Add("2");
+            comboBox1.Items.Add("3");
+            comboBox1.Items.Add("4");
+
+            asansör1 = rastgele.Next(1, 5);
+            asansör2 = rastgele.Next(1, 5);
+            label4.Text = asansör1.ToString();
+            label5.Text = asansör2.ToString();
+
+        }
+        int asansör1, asansör2, mevcutkat,fark1,fark2;
+        Random rastgele = new Random();
+
+        private void kat4_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void kat3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kat2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kat1_Click(object sender, EventArgs e)
+        {   
+            fark1 = mevcutkat - asansör1;
+            fark2 = mevcutkat - asansör2;
+            if (fark1 < fark2)
+            {
+                asansör1 = mevcutkat;
+            }
+            else if (fark2 < fark1)
+            {
+                asansör2 = mevcutkat;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fark1 = 0;
+            fark2 = 0;
+            if (comboBox1.Text == "")
+            {
+                MessageBox.Show("Lütfen Kat Seçiniz");
+            }
+            else
+            {
+                mevcutkat = int.Parse(comboBox1.Text);
+            }
+            kat1.Enabled = true;
+            kat2.Enabled = true;
+            kat3.Enabled = true;
+            kat4.Enabled = true;
+
+            label4.Text = asansör1.ToString();
+            label5.Text = asansör2.ToString();
+            
+            if (mevcutkat == 1)
+            {
+                kat1.Enabled = false;
+            }
+            if (mevcutkat == 2)
+            {
+                kat2.Enabled = false;
+            }
+            if (mevcutkat == 3)
+            {
+                kat3.Enabled = false;
+            }
+            if (mevcutkat == 4)
+            {
+                kat4.Enabled = false;
+            }
+            fark1 = mevcutkat - asansör1;
+            fark2 = mevcutkat - asansör2;
+            if (fark1 < fark2)
+            {
+                asansör1 = mevcutkat;
+            }
+            else if (fark2 < fark1)
+            {
+                asansör2 = mevcutkat;
+            }
+            else
+            {
+               int x = rastgele.Next(0,2);
+                if (x==0)
+                {
+                    asansör1 = mevcutkat;
+                }
+                else
+                {
+                    asansör2 = mevcutkat;
+                }
+            }
+        }
+
+    }
+}
